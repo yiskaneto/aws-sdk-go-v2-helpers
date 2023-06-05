fc86b74dac33ec0b575e70bb23cef38ed74dcaa5
Escanoru <44711170+escanoru@users.noreply.github.com>
2023-06-05 17:14:38 +0000

    return error for better handling tests

--- helper_errors/helper_errors.go
+++ helper_errors/helper_errors.go
@@ -2,22 +2,22 @@ package helper_errors
 
 import (
 	"errors"
-	"log"
+	"fmt"
 
 	awshttp "github.com/aws/aws-sdk-go-v2/aws/transport/http"
 	"github.com/aws/smithy-go"
 )
 
-func CheckAWSError(err error) {
+func CheckAWSError(err error) error {
 	if err != nil {
 		var ae smithy.APIError
 		var re *awshttp.ResponseError
 		if errors.As(err, &ae) {
-			log.Printf("Failure Code: %s, Message: %s, Fault is on: %s", ae.ErrorCode(), ae.ErrorMessage(), ae.ErrorFault().String())
-			if errors.As(err, &re) {
-				log.Fatalf("requestID: %s", re.ServiceRequestID())
-			}
+			return fmt.Errorf("Failure Code: %s, Message: %s, Fault is on: %s", ae.ErrorCode(), ae.ErrorMessage(), ae.ErrorFault().String())
 		}
-		return
+		if errors.As(err, &re) {
+			return fmt.Errorf("requestID: %s", re.ServiceRequestID())
+		}
 	}
+	return nil
 }
